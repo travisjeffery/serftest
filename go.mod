3c65a85f8db2281f01728e201f092ee5ff00bba9
Travis Jeffery <tj@travisjeffery.com>
2019-06-08 22:08:36 -0400

    cut everything down

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/travisjeffery/serftest
 go 1.12
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/gorilla/mux v1.7.2
 	github.com/hashicorp/serf v0.8.3
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
--- main.go
+++ main.go
@@ -1,193 +1,71 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/serf/serf"
-	"golang.org/x/sync/errgroup"
 )
 
-type oneAndOnlyNumber struct {
-	num        int
-	generation int
-	numMutex   sync.Mutex
-}
-
-func InitTheNumber(val int) *oneAndOnlyNumber {
-	return &oneAndOnlyNumber{num: val}
-}
-
-func (n *oneAndOnlyNumber) setVal(val int) {
-	n.numMutex.Lock()
-	defer n.numMutex.Unlock()
-	n.num = val
-	n.generation++
-}
-
-func (n *oneAndOnlyNumber) getVal() (int, int) {
-	n.numMutex.Lock()
-	defer n.numMutex.Unlock()
-	return n.num, n.generation
-}
-
-func (n *oneAndOnlyNumber) notifyValue(curVal int, curGeneration int) bool {
-	if curGeneration > n.generation {
-		n.numMutex.Lock()
-		defer n.numMutex.Unlock()
-		n.generation = curGeneration
-		n.num = curVal
-		return true
-	}
-	return false
-}
-
 func main() {
-	advertiseAddr := os.Getenv("ADVERTISE_ADDR")
-	clusterAddr := os.Getenv("CLUSTER_ADDR")
-	log.Printf("advertise addr: %s, cluster addr: %s\n", advertiseAddr, clusterAddr)
-	cluster, err := setupCluster(advertiseAddr, clusterAddr)
-	if err != nil {
-		log.Fatal(err)
+	srv := &server{}
+	if err := srv.setupSerf(); err != nil {
+		panic(err)
 	}
-	defer cluster.Leave()
+	go srv.logState()
+	srv.eventHandler()
+}
 
-	theOneAndOnlyNumber := InitTheNumber(42)
-	go launchHTTPAPI(theOneAndOnlyNumber)
-	ctx := context.Background()
-	name, err := os.Hostname()
+type server struct {
+	serf   *serf.Serf
+	events chan serf.Event
+}
+
+func (s *server) setupSerf() error {
+	cfg := serf.DefaultConfig()
+	cfg.Init()
+	cfg.NodeName = os.Getenv("NODE_NAME")
+	portStr := os.Getenv("NODE_PORT")
+	port, err := strconv.Atoi(portStr)
 	if err != nil {
 		panic(err)
 	}
-	ctx = context.WithValue(ctx, "name", name)
-	debugDataPrintTicker := time.Tick(time.Second * 5)
-	numberBroadcastTicker := time.Tick(time.Second * 2)
-	for {
-		select {
-		case <-numberBroadcastTicker:
-			members := getOtherMembers(cluster)
-			ctx, _ := context.WithTimeout(ctx, time.Second*2)
-			log.Printf("%s: notify others: %d\n", name, len(members))
-			go notifyOthers(ctx, members, theOneAndOnlyNumber)
-		case <-debugDataPrintTicker:
-			log.Printf("%s: members: %v\n", name, cluster.Members())
-			curVal, curGen := theOneAndOnlyNumber.getVal()
-			log.Printf("%s: state: val: %v gen: %v\n", name, curVal, curGen)
-		}
-	}
-}
-
-func getOtherMembers(cluster *serf.Serf) []serf.Member {
-	members := cluster.Members()
-	for i := 0; i < len(members); {
-		if members[i].Name == cluster.LocalMember().Name || members[i].Status != serf.StatusAlive {
-			if i < len(members)-1 {
-				members = append(members[:i], members[i+1:]...)
-
-			} else {
-				members = members[:i]
-			}
-		} else {
-			i++
-		}
-	}
-	return members
-}
-
-func notifyOthers(ctx context.Context, otherMembers []serf.Member, db *oneAndOnlyNumber) {
-	g, ctx := errgroup.WithContext(ctx)
-	for _, member := range otherMembers {
-		curMember := member
-		g.Go(func() error {
-			return notifyMember(ctx, curMember.Addr.String(), db)
-		})
-	}
-	err := g.Wait()
-	if err != nil {
-		log.Printf("error when notify other members: %v", err)
-	}
-}
-
-func notifyMember(ctx context.Context, addr string, db *oneAndOnlyNumber) error {
-	val, gen := db.getVal()
-	notifier := ctx.Value("name")
-	log.Printf("%s: notify member: addr: %s val: %v gen: %v", notifier, addr, val, gen)
-	req, err := http.NewRequest("POST",
-		fmt.Sprintf("http://%v:8080/notify/%v/%v?notifier=%v", addr, val, gen), nil)
+	cfg.MemberlistConfig.BindPort = port
+	cfg.MemberlistConfig.BindAddr = os.Getenv("NODE_ADDR")
+	s.events = make(chan serf.Event)
+	cfg.EventCh = s.events
+	s.serf, err = serf.Create(cfg)
 	if err != nil {
 		return err
 	}
-	req = req.WithContext(ctx)
-	_, err = http.DefaultClient.Do(req)
-	return err
-}
-
-func setupCluster(advertiseAddr, clusterAddr string) (*serf.Serf, error) {
-	conf := serf.DefaultConfig()
-	conf.Init()
-	conf.MemberlistConfig.AdvertiseAddr = advertiseAddr
-
-	cluster, err := serf.Create(conf)
-	if err != nil {
-		return nil, err
-	}
-
-	if clusterAddr != "" {
-		_, err = cluster.Join([]string{clusterAddr}, true)
-		if err != nil {
-			return nil, err
+	if os.Getenv("CLUSTER_ADDR") != "" {
+		if _, err = s.serf.Join([]string{os.Getenv("CLUSTER_ADDR")}, true); err != nil {
+			return err
 		}
 	}
-
-	return cluster, nil
+	return nil
 }
 
-func launchHTTPAPI(db *oneAndOnlyNumber) {
-	m := mux.NewRouter()
+func (s *server) eventHandler() {
+	for event := range s.events {
+		fmt.Printf("serf event: %v", event)
+	}
+}
 
-	m.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
-		val, _ := db.getVal()
-		fmt.Fprintf(w, "%v", val)
-	})
-
-	m.HandleFunc("/set/{newVal}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		newVal, err := strconv.Atoi(vars["newVal"])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "%v", err)
-			return
+func (s *server) logState() {
+	for {
+		time.Sleep(1 * time.Second)
+		var buf bytes.Buffer
+		buf.WriteString("members:\n")
+		for _, member := range s.serf.Members() {
+			buf.WriteString(fmt.Sprintf("member: %s\n", member.Name))
+			buf.WriteString(spew.Sdump(member))
+			buf.WriteString("\n")
 		}
-		db.setVal(newVal)
-		fmt.Fprintf(w, "%v", newVal)
-	})
-
-	m.HandleFunc("/notify/{curVal}/{curGeneration}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		curVal, err := strconv.Atoi(vars["curVal"])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "%v", err)
-			return
-		}
-		curGeneration, err := strconv.Atoi(vars["curGeneration"])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "%v", err)
-			return
-		}
-
-		if changed := db.notifyValue(curVal, curGeneration); changed {
-			log.Printf("notify: val: %v, gen: %v, notifier: %v", curVal, curGeneration, r.URL.Query().Get("notifier"))
-		}
-		w.WriteHeader(http.StatusOK)
-	})
-	log.Fatal(http.ListenAndServe(":8080", m))
+		fmt.Println(buf.String())
+	}
 }
